25948673872e591a1e23929547ef5afc555466ef
Shubham <8935shubham@ece.du.ac.in>
2025-03-03 01:23:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("contact-form").addEventListener("submit", function(event) {
     event.preventDefault();
-    document.getElementById("response").innerText = "Message sent!";
-    document.getElementById("response").style.color = "cyan";
+    document.getElementById("response").innerText = "Message sent successfully!";
+    document.getElementById("response").style.color = "green";
     this.reset();
 });
